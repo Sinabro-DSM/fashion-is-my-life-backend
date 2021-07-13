a033c2127c1486a90ef2d7ad70f8a1eaa9b13594
Seung jun <poodle2405@gmail.com>
2021-07-13 19:15:03 +0900

    ⚡️ :: import typeorm config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmConfigModule } from './typerom/typeorm-config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
