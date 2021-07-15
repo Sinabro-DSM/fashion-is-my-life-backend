4d2d6a6ceaaaba645e05262121ee671d0bd4e835
Seung jun <poodle2405@gmail.com>
2021-07-15 16:31:33 +0900

    ⚡️ :: post entity

---
+++ src/shared/entity/post/post.entity.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Comment } from '../comment/comment.entity';
+import { Hashtag } from '../hashtag/hashtag.entity';
+import { User } from '../user/user.entity';
+
+@Entity('post')
+export class Post {
+  @PrimaryGeneratedColumn()
+  post_id: number;
+
+  @Column({ length: 50 })
+  title: string;
+
+  @Column({ length: 256, nullable: true })
+  content: string;
+
+  @Column({ length: 100 })
+  picture: string;
+
+  @Column({ length: 30, nullable: true })
+  top_info: string;
+
+  @Column({ length: 30, nullable: true })
+  bottoms_info: string;
+
+  @Column({ length: 30, nullable: true })
+  shoes_info: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ name: 'user_id' })
+  user_id: number;
+
+  @ManyToOne((type) => User, (user) => user.user_id, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany((type) => Hashtag, (hashtag) => hashtag.post)
+  hashtag: Hashtag[];
+
+  @OneToMany((type) => Comment, (comment) => comment.post)
+  commnet: Comment[];
+}
