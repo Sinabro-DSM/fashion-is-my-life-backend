f27424af3dd114fcfef963386ab65b1e5ade7575
Seung jun <poodle2405@gmail.com>
2021-07-22 12:08:37 +0900

    ⚡️ :: import app module
    postmodule, closetmodule, usermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmConfigModule } from './typerom/typeorm-config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { ClosetModule } from './closet/closet.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmConfigModule],
+  imports: [TypeOrmConfigModule, PostModule, ClosetModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
