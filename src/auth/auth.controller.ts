4a2f8f56cab2347ac3863f47c93ca76d62631f1e
Seung jun <poodle2405@gmail.com>
2021-07-24 17:22:45 +0900

    ⚡️ :: auth controller
    update signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { User } from 'src/shared/entity/user/user.entity';
+import { SignUpDto } from './dto/signup.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  public async signUp(@Body() user: SignUpDto) {
+    await this.authService.signUp(user);
+    return { status: 200, message: 'success' };
+  }
+}
