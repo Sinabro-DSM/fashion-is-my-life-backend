25f7245af2cdba318864512272203ffc0470a7da
Seung jun <poodle2405@gmail.com>
2021-07-24 17:13:50 +0900

    ⚡️ :: user repository

--- src/shared/entity/user/user.repository.ts
+++ src/shared/entity/user/user.repository.ts
@@ -1,5 +1,12 @@
+import { SignUpDto } from 'src/auth/dto/signup.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(user: SignUpDto): Promise<User> {
+    let newUser: User;
+    newUser = this.create(user);
+    return await this.save(newUser);
+  }
+}
