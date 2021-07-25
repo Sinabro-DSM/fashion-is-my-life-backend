65584016a014f93798c0bd12d7ad85f819581698
Seung jun <poodle2405@gmail.com>
2021-07-25 18:18:37 +0900

    ⚡️ :: exception index file

--- src/shared/exception/exception.index.ts
+++ src/shared/exception/exception.index.ts
@@ -6,6 +6,6 @@ import {
 
 export const badRequestException = new BadRequestException();
 
-export const ExistEmailError = new ConflictException('Email already exist');
-
 export const notFoundEmailException = new NotFoundException('Not Found Email');
+
+export const ExistEmailError = new ConflictException('Email already exist');
