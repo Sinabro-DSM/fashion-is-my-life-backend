1ffee75503bd5b83a2e1ad78e83d6785d5393853
Seung jun <poodle2405@gmail.com>
2021-07-30 13:35:29 +0900

    ⚡️ :: closet controller
    create closet function

--- src/closet/closet.controller.ts
+++ src/closet/closet.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ClosetService } from './closet.service';
+import { closetResponseData } from './dto/closet.dto';
 
 @Controller('closet')
-export class ClosetController {}
+export class ClosetController {
+  constructor(private readonly closetService: ClosetService) {}
+
+  @Get(':userId')
+  public async closet(
+    @Param('user_id') user_id: number,
+  ): Promise<closetResponseData> {
+    return await this.closetService.closet(user_id);
+  }
+}
