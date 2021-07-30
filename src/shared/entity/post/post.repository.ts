c0feffc117e117d7a754d7842066b8251bb66bdd
Seung jun <poodle2405@gmail.com>
2021-07-30 13:39:19 +0900

    ⚡️ :: post repository
    update closet querybuilder

--- src/shared/entity/post/post.repository.ts
+++ src/shared/entity/post/post.repository.ts
@@ -1,5 +1,21 @@
+import { closetResponseData } from 'src/closet/dto/closet.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { Post } from './post.entity';
 
 @EntityRepository(Post)
-export class PostRepository extends Repository<Post> {}
+export class PostRepository extends Repository<Post> {
+  async closet(user_id: number): Promise<closetResponseData> {
+    return this.createQueryBuilder('post')
+      .innerJoin('post.user_id', 'user')
+      .innerJoin('post.hashtag', 'hashtag')
+      .innerJoin('post.hanger_exis', 'hanger')
+      .select('user.nickname', 'nickname')
+      .addSelect('post.title', 'title')
+      .addSelect('post.picture', 'picture')
+      .addSelect('hashtag.name', 'hashtag')
+      .addSelect('hanger.user_id', 'hanger')
+      .addSelect('post.createAt', 'createAt')
+      .where('hanger.user_id = user_id', { user_id })
+      .getMany() as closetResponseData;
+  }
+}
