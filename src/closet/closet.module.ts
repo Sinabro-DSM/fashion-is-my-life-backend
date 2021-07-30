a3e078a0bf8a7c1f08545242e4c201b7e7d6ff4e
Seung jun <poodle2405@gmail.com>
2021-07-30 13:36:07 +0900

    ⚡️ :: closet module
    import TypeOrmModule

--- src/closet/closet.module.ts
+++ src/closet/closet.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClosetService } from './closet.service';
 import { ClosetController } from './closet.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostRepository } from 'src/shared/entity/post/post.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostRepository])],
   providers: [ClosetService],
-  controllers: [ClosetController]
+  controllers: [ClosetController],
 })
 export class ClosetModule {}
