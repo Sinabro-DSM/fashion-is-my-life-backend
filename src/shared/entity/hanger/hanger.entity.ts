9a501f70aee791e0b6f18baf0b6eee695600c0f2
Seung jun <poodle2405@gmail.com>
2021-07-30 13:37:16 +0900

    ⚡️ :: hanger entity
    add post

--- src/shared/entity/hanger/hanger.entity.ts
+++ src/shared/entity/hanger/hanger.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { Post } from '../post/post.entity';
 
 @Entity('hanger')
 export class Hanger {
@@ -7,4 +8,7 @@ export class Hanger {
 
   @PrimaryColumn()
   post_id: number;
+
+  @OneToMany((type) => Post, (post) => post.hanger)
+  post: Post[];
 }
