60358af99762372d4038e28869ad3e9b81f11778
Seung jun <poodle2405@gmail.com>
2021-07-31 16:44:37 +0900

    ⚡️ :: import typeorm

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostRepository } from 'src/shared/entity/post/post.repository';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostRepository])],
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService],
 })
 export class PostModule {}
