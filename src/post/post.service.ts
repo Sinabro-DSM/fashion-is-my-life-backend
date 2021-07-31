8b0a6480efab3f098b6de8ec11e1654ce8012ee4
Seung jun <poodle2405@gmail.com>
2021-07-31 16:48:35 +0900

    ⚡️ :: post service
    update createPost, deletePost func

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from 'src/shared/entity/post/post.entity';
+import { PostRepository } from 'src/shared/entity/post/post.repository';
+import { postRequestData } from './dto/post-req.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(Post) private readonly postRepository: PostRepository,
+  ) {}
+
+  public async createPost(@Body() post: postRequestData) {
+    return await this.postRepository.createPost(post);
+  }
+
+  public async deletePost(post_id: number) {
+    return await this.postRepository.deletePost(post_id);
+  }
+}
