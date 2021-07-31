4d968d39423acf8c867f95a215b9c400351fa6e9
Seung jun <poodle2405@gmail.com>
2021-07-31 16:48:06 +0900

    ⚡️ :: post controller
    update createPost, deletePost func

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { postRequestData } from './dto/post-req.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  public async createPost(@Body() post: postRequestData) {
+    await this.postService.createPost(post);
+    return { status: 200, message: 'success' };
+  }
+
+  @Delete(':postId')
+  public async deletePost(@Param('post_id') post_id: number) {
+    await this.postService.deletePost(post_id);
+    return { status: 200, message: 'success' };
+  }
+}
