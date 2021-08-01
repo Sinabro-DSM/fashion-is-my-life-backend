ad630d39501480d22a14dfea7635574d67f66c16
Seung jun <poodle2405@gmail.com>
2021-08-01 21:24:01 +0900

    ⚡️ :: post module
    import multermodule

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostRepository } from 'src/shared/entity/post/post.repository';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostRepository])],
+  imports: [
+    MulterModule.register({
+      dest: './upload',
+    }),
+    TypeOrmModule.forFeature([PostRepository]),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
