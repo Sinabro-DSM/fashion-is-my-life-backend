e26b01b82f3f052cd2fb05ca9c3e1033b52677d4
Seung jun <poodle2405@gmail.com>
2021-08-05 18:21:42 +0900

    ⚡️ :: auth controller
    update emailAuthentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from 'src/shared/entity/user/user.entity';
 import { SignUpDto } from './dto/signup.dto';
+import { checkEmailDto } from './dto/check-email.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,10 @@ export class AuthController {
   public async signUp(@Body() user: SignUpDto) {
     await this.authService.signUp(user);
     return { status: 200, message: 'success' };
+  }
+
+  @Post('/email')
+  public async emailAuthentication(@Body() body: checkEmailDto) {
+    return await this.authService.sendMail(body.email);
   }
 }
