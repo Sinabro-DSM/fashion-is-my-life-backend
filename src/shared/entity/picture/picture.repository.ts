07f038a582b686f45a9186701ef71e9d3675fed0
Seung jun <poodle2405@gmail.com>
2021-09-10 20:11:57 +0900

    ⚡️ :: picture repository
    create savePicture function

--- src/shared/entity/picture/picture.repository.ts
+++ src/shared/entity/picture/picture.repository.ts
@@ -2,4 +2,15 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Picture } from './picture.entity';
 
 @EntityRepository(Picture)
-export class PictureRepository extends Repository<Picture> {}
+export class PictureRepository extends Repository<Picture> {
+  async savePicture(file_path: string) {
+    let newPicture: Picture;
+
+    newPicture = this.create({
+      picture_path: file_path,
+    });
+    let picture = await this.save(newPicture);
+
+    return picture;
+  }
+}
