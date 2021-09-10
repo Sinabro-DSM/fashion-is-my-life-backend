976344c772258ee76ea9e0b2e9671f7ada1ef490
Seung jun <poodle2405@gmail.com>
2021-09-10 16:44:08 +0900

    :bookmark_tabs: :: picture entity

---
+++ src/shared/entity/picture/picture.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Post } from '../post/post.entity';
+
+@Entity('picture')
+export class Picture {
+  @PrimaryGeneratedColumn()
+  picture_id: number;
+
+  @Column({ length: 256 })
+  picture_path: string;
+
+  @ManyToOne((type) => Post, (post) => post.post_id, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'post_id' })
+  post: Post;
+}
