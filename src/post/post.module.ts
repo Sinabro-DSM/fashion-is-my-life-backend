1c230965f3fdb344c0d810619cf523237136b751
Seung jun <poodle2405@gmail.com>
2021-09-12 22:40:51 +0900

    ⚡️ :: post module
    improt PictureRepository

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -4,13 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostRepository } from 'src/shared/entity/post/post.repository';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { PictureRepository } from 'src/shared/entity/picture/picture.repository';
 
 @Module({
   imports: [
     MulterModule.register({
       dest: './upload',
     }),
-    TypeOrmModule.forFeature([PostRepository]),
+    TypeOrmModule.forFeature([PostRepository, PictureRepository]),
   ],
   controllers: [PostController],
   providers: [PostService],
