f75c0c220d1189d39522e577214badcd3cf104e6
Seung jun <poodle2405@gmail.com>
2021-09-12 23:18:05 +0900

    ⚡️ :: comment service
    deleteComment function

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+<<<<<<< Updated upstream
 
 @Injectable()
 export class CommentService {}
+=======
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comment } from 'src/shared/entity/comment/comment.entity';
+import { CommentRepository } from 'src/shared/entity/comment/comment.repository';
+
+@Injectable()
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: CommentRepository,
+  ) {}
+
+  public async deleteComment(comment_id: number) {
+    return await this.commentRepository.deleteComment(comment_id);
+  }
+}
+>>>>>>> Stashed changes
