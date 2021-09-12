67787b856abb381ebaf52b73d20e2c91a6e99427
Seung jun <poodle2405@gmail.com>
2021-09-12 23:32:26 +0900

    ⚡️ :: user service
    init

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/shared/entity/user/user.entity';
+import { UserRepository } from 'src/shared/entity/user/user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: UserRepository,
+  ) {}
+}
