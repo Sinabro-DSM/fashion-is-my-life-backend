c65a6d6635419db25a8ab92a81ff3f2af0b4cd91
Seung jun <poodle2405@gmail.com>
2021-09-12 23:17:39 +0900

    ⚡️ :: comment module

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
-<<<<<<< Updated upstream
-=======
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentRepository } from 'src/shared/entity/comment/comment.repository';
->>>>>>> Stashed changes
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 
 @Module({
-<<<<<<< Updated upstream
-  controllers: [CommentController],
-  providers: [CommentService]
-=======
   imports: [TypeOrmModule.forFeature([CommentRepository])],
   controllers: [CommentController],
   providers: [CommentService],
->>>>>>> Stashed changes
 })
 export class CommentModule {}
