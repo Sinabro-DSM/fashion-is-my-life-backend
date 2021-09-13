6240e6edebc32df92153b51d5ceb7e6eb101e5b7
Seung jun <poodle2405@gmail.com>
2021-09-13 11:01:50 +0900

    ⚡️ :: user controller
    modifyProfile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Put,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ProfileMulterOptions } from 'src/config/multer';
+import { modifyProfileDto } from './dto/modifyProfile.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Put()
+  @UseInterceptors(FileInterceptor('profile', ProfileMulterOptions))
+  public async modifyProfile(
+    @UploadedFile() profile: Express.Multer.File,
+    @Body() modifyProfileDto: modifyProfileDto,
+  ) {
+    await this.userService.modifyProfile(profile.filename, modifyProfileDto);
+    return { status: 200, message: 'success' };
+  }
 }
