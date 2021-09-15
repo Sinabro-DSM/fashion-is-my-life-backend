0648d2fe3640e520880c0d8aec57ffbd4d39463e
Seung jun <poodle2405@gmail.com>
2021-09-15 14:24:30 +0900

    ⚡️ :: comment service
    getComment API

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -10,6 +10,10 @@ export class CommentService {
     private readonly commentRepository: CommentRepository,
   ) {}
 
+  public async getComment(post_id: number) {
+    return await this.commentRepository.getComment(post_id);
+  }
+
   public async deleteComment(comment_id: number) {
     return await this.commentRepository.deleteComment(comment_id);
   }
