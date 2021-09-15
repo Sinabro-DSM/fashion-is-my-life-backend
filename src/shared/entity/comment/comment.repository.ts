51dfa4be96f9312eb7b546bd42f1c0a3fb3f623d
Seung jun <poodle2405@gmail.com>
2021-09-15 14:23:58 +0900

    ⚡️ :: comment repository
    getComment API

--- src/shared/entity/comment/comment.repository.ts
+++ src/shared/entity/comment/comment.repository.ts
@@ -3,11 +3,21 @@ import { Comment } from './comment.entity';
 
 @EntityRepository(Comment)
 export class CommentRepository extends Repository<Comment> {
+  async getComment(post_id: number) {
+    return this.createQueryBuilder('comment')
+      .innerJoin('comment.user_id', 'user')
+      .select('user.user_id', 'user_id')
+      .addSelect('user.profile_path', 'profile_path')
+      .addSelect('comment.comment', 'comment')
+      .where('post_id = :post_id', { post_id })
+      .getMany();
+  }
+
   async deleteComment(comment_id: number) {
     return this.createQueryBuilder('comment')
       .delete()
       .from(Comment)
-      .where('comment_id = comment_id', { comment_id })
+      .where('comment_id = :comment_id', { comment_id })
       .execute();
   }
 }
