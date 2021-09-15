765e82f4f651b43e93d019fd6f2d568c28af57e7
Seung jun <poodle2405@gmail.com>
2021-09-15 14:41:10 +0900

    ⚡️ :: comment service
    createComment API

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -14,6 +14,10 @@ export class CommentService {
     return await this.commentRepository.getComment(post_id);
   }
 
+  public async createComment(post_id: number, comment: string) {
+    return await this.commentRepository.createComment(post_id, comment);
+  }
+
   public async deleteComment(comment_id: number) {
     return await this.commentRepository.deleteComment(comment_id);
   }
