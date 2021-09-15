c8e4729c987a4e9e0e55f26e75498a34c57695e6
Seung jun <poodle2405@gmail.com>
2021-09-15 14:24:49 +0900

    ⚡️ :: comment controller
    getComment API

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Delete, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { CommentService } from './comment.service';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  @Get()
+  public async getComment(@Param('post_id') post_id: number) {
+    await this.commentService.getComment(post_id);
+    return { status: 200, message: 'success' };
+  }
 
   @Delete(':commentId')
   public async deleteComment(@Param('comment_id') comment_id: number) {
