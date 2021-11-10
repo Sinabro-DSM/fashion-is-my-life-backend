6ea70a25593d1cc08756be73775f950596c343ae
Seung jun <poodle2405@gmail.com>
2021-11-10 15:52:22 +0900

    [DELETE] auth service
    delete HttpException, HttpStatus

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MailerService } from '@nestjs-modules/mailer';
 import { User } from 'src/shared/entity/user/user.entity';
