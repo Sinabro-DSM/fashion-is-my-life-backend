92bfdf0b7dc97d2035593920709fd70284ce1bf6
Seung jun <poodle2405@gmail.com>
2021-11-15 17:07:37 +0900

    UPDATE: post repository
    Correct spelling errors

--- src/shared/entity/post/post.repository.ts
+++ src/shared/entity/post/post.repository.ts
@@ -15,7 +15,7 @@ export class PostRepository extends Repository<Post> {
       .addSelect('picture.picture_path', 'picture_path')
       .addSelect('hashtag.name', 'name')
       .addSelect('hanger.user_id', 'user_id')
-      .addSelect('post.createAt', 'createAt')
+      .addSelect('post.createdAt', 'createdAt')
       .where('hanger.user_id = :user_id', { user_id: user_id })
       .getMany() as closetResponseData;
   }
@@ -49,15 +49,15 @@ export class PostRepository extends Repository<Post> {
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
-      .addSelect('post.createdAt', 'createAt')
+      .addSelect('post.createdAt', 'createdAt')
       .from(Post, 'post')
       .orderBy('RANDOM()')
       .limit(15)
       .execute();
   }
 
-  //좋아요순 게시물 
-  async postLike(){
+  //좋아요순 게시물
+  async postLike() {
     return this.createQueryBuilder('post')
       .innerJoin('post.picture', 'picture')
       .innerJoin('post.hanger', 'hanger')
@@ -67,14 +67,14 @@ export class PostRepository extends Repository<Post> {
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
-      .addSelect('post.createdAt', 'createAt')
+      .addSelect('post.createdAt', 'createdAt')
       .from(Post, 'post')
       .select('COUNT(*) AS hangerCount')
       .limit(15)
       .execute();
   }
 
-  async getAllpost(){
+  async getAllpost() {
     return this.createQueryBuilder('post')
       .innerJoin('post.hashtage', 'hashtag')
       .innerJoin('post.picture', 'picture')
@@ -84,14 +84,13 @@ export class PostRepository extends Repository<Post> {
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
-      .addSelect('post.createdAt', 'createAt')
+      .addSelect('post.createdAt', 'createdAt')
       .from(Post, 'post')
-      .getMany()
-      
+      .getMany();
   }
-  
+
   //최신순 게시물
-  async postRecency(){
+  async postRecency() {
     return this.createQueryBuilder('post')
       .innerJoin('post.picture', 'picture')
       .innerJoin('post.hanger', 'hanger')
@@ -101,7 +100,7 @@ export class PostRepository extends Repository<Post> {
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
-      .addSelect('post.createdAt', 'createAt')
+      .addSelect('post.createdAt', 'createdAt')
       .from(Post, 'post')
       .orderBy('post.createdAt', 'DESC')
       .limit(15)
@@ -119,7 +118,7 @@ export class PostRepository extends Repository<Post> {
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
-      .addSelect('post.createAt', 'createAt')
+      .addSelect('post.createdAt', 'createdAt')
       .where('hashtag.name like %:name% OR user.nickname like %:nickname%', {
         name: searchWord,
         nickname: searchWord,
@@ -137,7 +136,7 @@ export class PostRepository extends Repository<Post> {
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
-      .addSelect('post.createAt', 'createAt')
+      .addSelect('post.createdAt', 'createdAt')
       .where('post.post_id = :post_id', { post_id: post_id })
       .getOne();
   }
