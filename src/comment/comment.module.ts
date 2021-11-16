f9ea384842824d2e6cfbbd7d742d824d55f22b33
Seung jun <poodle2405@gmail.com>
2021-11-16 23:14:00 +0900

    UPDATE: comment module
    import UserRepository

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentRepository } from 'src/shared/entity/comment/comment.repository';
+import { UserRepository } from 'src/shared/entity/user/user.repository';
 import { JwtStrategy } from 'src/shared/jwt/passport/jwt.strategy';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentRepository])],
+  imports: [TypeOrmModule.forFeature([CommentRepository, UserRepository])],
   controllers: [CommentController],
   providers: [CommentService, JwtStrategy],
 })
