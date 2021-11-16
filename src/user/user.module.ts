fe7345fde59f223f0a06602a527a66164a318bf5
Seung jun <poodle2405@gmail.com>
2021-11-16 22:19:26 +0900

    UPDATE: uset module
    providers jwtStrategy

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/shared/entity/user/user.repository';
+import { JwtStrategy } from 'src/shared/jwt/passport/jwt.strategy';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtStrategy],
+  exports: [UserService],
 })
 export class UserModule {}
