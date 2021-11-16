68cade723c0273df3418eda9fa57c5f827ade842
Seung jun <poodle2405@gmail.com>
2021-11-16 23:39:20 +0900

    UPDATE: picrute repository

--- src/shared/entity/picture/picture.repository.ts
+++ src/shared/entity/picture/picture.repository.ts
@@ -3,10 +3,11 @@ import { Picture } from './picture.entity';
 
 @EntityRepository(Picture)
 export class PictureRepository extends Repository<Picture> {
-  async savePicture(file_path: string) {
+  async savePicture(file_path: string, post_id: number) {
     let newPicture: Picture;
 
     newPicture = this.create({
+      post_id: post_id,
       picture_path: file_path,
     });
     let picture = await this.save(newPicture);
