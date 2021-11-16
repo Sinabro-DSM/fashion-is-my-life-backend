3c1866438c7929a614e062297d2caf80c3a937de
Seung jun <poodle2405@gmail.com>
2021-11-16 17:07:35 +0900

    UPDATE: exception index
    update notFoundUserException

--- src/shared/exception/exception.index.ts
+++ src/shared/exception/exception.index.ts
@@ -8,6 +8,8 @@ export const badRequestException = new BadRequestException();
 
 export const notFoundEmailException = new NotFoundException('Not Found Email');
 
+export const notFoundUserException = new NotFoundException('Unregistered user');
+
 export const ExistEmailException = new ConflictException('Email already exist');
 
 export const ExistUserException = new ConflictException('User already exist');
