3f4efcfaa4829960a670987d25dc45f4613539cd
Seung jun <poodle2405@gmail.com>
2021-11-16 22:20:35 +0900

    UPDATE: comment module
    providers jwtStrategy

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentRepository } from 'src/shared/entity/comment/comment.repository';
+import { JwtStrategy } from 'src/shared/jwt/passport/jwt.strategy';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CommentRepository])],
   controllers: [CommentController],
-  providers: [CommentService],
+  providers: [CommentService, JwtStrategy],
 })
 export class CommentModule {}
