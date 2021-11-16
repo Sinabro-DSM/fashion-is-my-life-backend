cbcf38279455815b87d3ac03854b5c67e8dd5b35
Seung jun <poodle2405@gmail.com>
2021-11-16 22:18:39 +0900

    UPDATE: closet module
    providers jwtStrategy

--- src/closet/closet.module.ts
+++ src/closet/closet.module.ts
@@ -3,10 +3,11 @@ import { ClosetService } from './closet.service';
 import { ClosetController } from './closet.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostRepository } from 'src/shared/entity/post/post.repository';
+import { JwtStrategy } from 'src/shared/jwt/passport/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PostRepository])],
-  providers: [ClosetService],
+  providers: [ClosetService, JwtStrategy],
   controllers: [ClosetController],
 })
 export class ClosetModule {}
