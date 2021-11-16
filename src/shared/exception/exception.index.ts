8677f2f6516605a047f7d29a83367d7edba0c6fb
Seung jun <poodle2405@gmail.com>
2021-11-16 17:13:00 +0900

    UPDATE: exception index
    update notConfirmPassword

--- src/shared/exception/exception.index.ts
+++ src/shared/exception/exception.index.ts
@@ -6,6 +6,10 @@ import {
 
 export const badRequestException = new BadRequestException();
 
+export const notConfirmPassword = new BadRequestException(
+  'Not Confirm Password',
+);
+
 export const notFoundEmailException = new NotFoundException('Not Found Email');
 
 export const notFoundUserException = new NotFoundException('Unregistered user');
