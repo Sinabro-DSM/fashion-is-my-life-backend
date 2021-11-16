b63ef87734b28e82d78121ea30cef7b84bf23eea
Seung jun <poodle2405@gmail.com>
2021-11-16 22:20:04 +0900

    UPDATE: jwt strategy
    update validate

--- src/shared/jwt/passport/jwt.strategy.ts
+++ src/shared/jwt/passport/jwt.strategy.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/shared/entity/user/user.entity';
+import { UserRepository } from 'src/shared/entity/user/user.repository';
+import { badRequestException } from 'src/shared/exception/exception.index';
+import { Payload } from '../payload/jwt-payload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: UserRepository,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET_KEY,
       ignoreExpiration: false,
     });
+  }
+
+  async validate(payload: Payload) {
+    const user = await this.userRepository.findOne({
+      where: { user_id: payload.sub },
+    });
+
+    if (user) {
+      return user;
+    } else {
+      throw badRequestException;
+    }
   }
 }
