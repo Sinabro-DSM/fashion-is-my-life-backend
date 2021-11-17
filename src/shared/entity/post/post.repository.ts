40feda5f75269771ee6c5257f577a95bc8674234
Seung jun <poodle2405@gmail.com>
2021-11-18 00:10:52 +0900

    UPDATE: post repository

--- src/shared/entity/post/post.repository.ts
+++ src/shared/entity/post/post.repository.ts
@@ -6,16 +6,16 @@ import { Post } from './post.entity';
 export class PostRepository extends Repository<Post> {
   async closet(user_id: number): Promise<closetResponseData> {
     return this.createQueryBuilder('post')
-      .innerJoin('post.user_id', 'user')
-      .innerJoin('post.hashtag', 'hashtag')
-      .innerJoin('post.hanger', 'hanger')
-      .innerJoin('post.picture', 'picture')
       .select('user.nickname', 'nickname')
       .addSelect('post.title', 'title')
       .addSelect('picture.picture_path', 'picture_path')
       .addSelect('hashtag.name', 'name')
       .addSelect('hanger.user_id', 'user_id')
       .addSelect('post.createdAt', 'createdAt')
+      .innerJoin('post.user_id', 'user')
+      .innerJoin('post.hashtag', 'hashtag')
+      .innerJoin('post.hanger', 'hanger')
+      .innerJoin('post.picture', 'picture')
       .where('hanger.user_id = :user_id', { user_id: user_id })
       .getMany() as closetResponseData;
   }
@@ -41,14 +41,11 @@ export class PostRepository extends Repository<Post> {
 
   async postRecommendation() {
     return this.createQueryBuilder('post')
-      .innerJoin('post.picture', 'picture')
-      .innerJoin('post.hanger', 'hanger')
-      .innerJoin('post.hashtag', 'hashtag')
       .select('post.title', 'title')
-      .addSelect('picture.picture_path', 'picture_path')
-      .addSelect('hanger.post_id', 'post_id')
+      .leftJoinAndSelect('picture.picture_path', 'picture_path')
+      .leftJoinAndSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
-      .addSelect('hashtag.name', 'name')
+      .leftJoinAndSelect('hashtag.name', 'name')
       .addSelect('post.createdAt', 'createdAt')
       .from(Post, 'post')
       .orderBy('RANDOM()')
@@ -59,15 +56,15 @@ export class PostRepository extends Repository<Post> {
   //좋아요순 게시물
   async postLike() {
     return this.createQueryBuilder('post')
-      .innerJoin('post.picture', 'picture')
-      .innerJoin('post.hanger', 'hanger')
-      .innerJoin('post.hashtag', 'hashtag')
       .select('post.title', 'title')
       .addSelect('picture.picture_path', 'picture_path')
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
       .addSelect('post.createdAt', 'createdAt')
+      .innerJoin('post.picture', 'picture')
+      .innerJoin('post.hanger', 'hanger')
+      .innerJoin('post.hashtag', 'hashtag')
       .from(Post, 'post')
       .select('COUNT(*) AS hangerCount')
       .limit(15)
@@ -76,15 +73,15 @@ export class PostRepository extends Repository<Post> {
 
   async getAllpost() {
     return this.createQueryBuilder('post')
-      .innerJoin('post.hashtage', 'hashtag')
-      .innerJoin('post.picture', 'picture')
-      .innerJoin('post.hanger', 'hanger')
       .select('post.title', 'title')
       .addSelect('picture.picture_path', 'picture_path')
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
       .addSelect('post.createdAt', 'createdAt')
+      .innerJoin('post.hashtage', 'hashtag')
+      .innerJoin('post.picture', 'picture')
+      .innerJoin('post.hanger', 'hanger')
       .from(Post, 'post')
       .getMany();
   }
@@ -92,15 +89,15 @@ export class PostRepository extends Repository<Post> {
   //최신순 게시물
   async postRecency() {
     return this.createQueryBuilder('post')
-      .innerJoin('post.picture', 'picture')
-      .innerJoin('post.hanger', 'hanger')
-      .innerJoin('post.hashtag', 'hashtag')
       .select('post.title', 'title')
       .addSelect('picture.picture_path', 'picture_path')
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
       .addSelect('post.createdAt', 'createdAt')
+      .innerJoin('post.picture', 'picture')
+      .innerJoin('post.hanger', 'hanger')
+      .innerJoin('post.hashtag', 'hashtag')
       .from(Post, 'post')
       .orderBy('post.createdAt', 'DESC')
       .limit(15)
@@ -109,16 +106,16 @@ export class PostRepository extends Repository<Post> {
 
   async search(searchWord: string) {
     return this.createQueryBuilder('post')
-      .innerJoin('post.hashtag', 'hashtag')
-      .innerJoin('post.picture', 'picture')
-      .innerJoin('post.hanger', 'hanger')
-      .innerJoin('post.hashtag', 'hashtag')
       .select('post.title', 'title')
       .addSelect('picture.picture_path', 'picture_path')
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
       .addSelect('post.createdAt', 'createdAt')
+      .innerJoin('post.hashtag', 'hashtag')
+      .innerJoin('post.picture', 'picture')
+      .innerJoin('post.hanger', 'hanger')
+      .innerJoin('post.hashtag', 'hashtag')
       .where('hashtag.name like %:name% OR user.nickname like %:nickname%', {
         name: searchWord,
         nickname: searchWord,
@@ -128,15 +125,15 @@ export class PostRepository extends Repository<Post> {
 
   async getPost(post_id: number) {
     return this.createQueryBuilder('post')
-      .innerJoin('post.picture', 'picture')
-      .innerJoin('post.hanger', 'hanger')
-      .innerJoin('post.hashtag', 'hashtag')
       .select('post.title', 'title')
       .addSelect('picture.picture_path', 'picture_path')
       .addSelect('hanger.post_id', 'post_id')
       .addSelect('post.content', 'content')
       .addSelect('hashtag.name', 'name')
       .addSelect('post.createdAt', 'createdAt')
+      .innerJoin('post.picture', 'picture')
+      .innerJoin('post.hanger', 'hanger')
+      .innerJoin('post.hashtag', 'hashtag')
       .where('post.post_id = :post_id', { post_id: post_id })
       .getOne();
   }
