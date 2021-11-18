2e821e6a604e73c65fccf1d0e3e28801447f7155
AHYEON04 <ahyeon3590@gmail.com>
2021-11-18 14:12:04 +0900

    [UPDATE]main

--- src/main.ts
+++ src/main.ts
@@ -12,17 +12,7 @@ async function bootstrap() {
     }),
   );
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
+  app.enableCors({ origin: '*' });
   await app.listen(3001);
 }
 bootstrap();
