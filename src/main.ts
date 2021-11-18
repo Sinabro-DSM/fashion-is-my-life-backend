adddf2fb2184a04269f940cfcf7bd7ebfa2adeec
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-18 15:05:29 +0900

    [UPDATE] main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +11,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
