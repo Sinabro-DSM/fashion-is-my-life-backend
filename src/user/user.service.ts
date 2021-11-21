6f4cb9027c23f5012d96e4f44fb4199df317b5e0
Seung jun <poodle2405@gmail.com>
2021-11-22 01:18:56 +0900

    UPDATE: user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable, Param } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/shared/entity/user/user.entity';
 import { UserRepository } from 'src/shared/entity/user/user.repository';
-import { getUserInfo } from './dto/getUserInfo.dto';
 import { modifyProfileDto } from './dto/modifyProfile.dto';
 
 @Injectable()
