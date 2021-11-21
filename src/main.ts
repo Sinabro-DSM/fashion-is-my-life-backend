fb3603332540f05097bdddaf5a977f9e47bda726
Seung jun <poodle2405@gmail.com>
2021-11-22 00:21:41 +0900

    UPDATE: main

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-});
+  });
+
   await app.listen(3001);
 }
 bootstrap();
