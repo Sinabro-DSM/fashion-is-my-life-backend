4ce856a7f9db1b19aa6749e7c9330e1023e7444c
Seung jun <poodle2405@gmail.com>
2021-11-22 01:18:40 +0900

    UPDATE: user repository

--- src/shared/entity/user/user.repository.ts
+++ src/shared/entity/user/user.repository.ts
@@ -1,5 +1,4 @@
 import { SignUpDto } from 'src/auth/dto/sign-up.dto';
-import { getUserInfo } from 'src/user/dto/getUserInfo.dto';
 import { modifyProfileDto } from 'src/user/dto/modifyProfile.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
