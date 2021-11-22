0d77919dc497011044f4fa5d6b239601d5255e82
Seung jun <poodle2405@gmail.com>
2021-11-22 23:23:58 +0900

    UPDATE: picture repository

--- src/shared/entity/picture/picture.repository.ts
+++ src/shared/entity/picture/picture.repository.ts
@@ -1,14 +1,17 @@
+import { MulterFileInterface } from 'src/config/multer.interface';
 import { EntityRepository, Repository } from 'typeorm';
 import { Picture } from './picture.entity';
 
 @EntityRepository(Picture)
 export class PictureRepository extends Repository<Picture> {
-  async savePicture(file_path: string, post_id: number) {
+  async savePicture(file_path: MulterFileInterface, post_id: number) {
     let newPicture: Picture;
+
+    const picture_path = file_path.location;
 
     newPicture = this.create({
       picture_id: post_id,
-      picture_path: file_path,
+      picture_path: picture_path,
       post_id: post_id,
     });
     let picture = await this.save(newPicture);
