27149e189514f692893b3db2e63cb21a02631ffc
Seung jun <poodle2405@gmail.com>
2021-12-22 14:53:20 +0900

    UPDATE: post req dto
    update validator

--- src/post/dto/post-req.dto.ts
+++ src/post/dto/post-req.dto.ts
@@ -1,11 +1,30 @@
+import { IsArray, IsNumber, IsString } from 'class-validator';
+
 export class postRequestDto {
+  @IsString()
   title: string;
+
+  @IsString()
   topInfo?: string;
+
+  @IsString()
   bottomInfo?: string;
+
+  @IsString()
   shoesInfo?: string;
+
+  @IsString()
   content?: string;
+
+  @IsArray()
   tags?: string[];
+
+  @IsNumber()
   r?: number;
+
+  @IsNumber()
   g?: number;
+
+  @IsNumber()
   b?: number;
 }
