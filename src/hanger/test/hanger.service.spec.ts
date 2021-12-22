3d2114b5baac246748c86edaf63225b5efd6f2b9
Seung jun <poodle2405@gmail.com>
2021-12-22 14:54:55 +0900

    UPDATE: hanger service spec

--- src/hanger/test/hanger.service.spec.ts
+++ src/hanger/test/hanger.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HangerService } from './hanger.service';
+import { HangerService } from '../hanger.service';
 
 describe('HangerService', () => {
   let service: HangerService;
