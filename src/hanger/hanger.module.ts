c8ad9d48bd9d7b5d51efe13dd4ad65299e5dd14d
Seung jun <poodle2405@gmail.com>
2021-12-22 10:26:51 +0900

    UPDATE: hanger module

--- src/hanger/hanger.module.ts
+++ src/hanger/hanger.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HangerRepository } from 'src/shared/entity/hanger/hanger.repository';
+import { UserRepository } from 'src/shared/entity/user/user.repository';
 import { JwtStrategy } from 'src/shared/jwt/passport/jwt.strategy';
 import { HangerController } from './hanger.controller';
 import { HangerService } from './hanger.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HangerRepository])],
+  imports: [TypeOrmModule.forFeature([HangerRepository, UserRepository])],
   controllers: [HangerController],
   providers: [HangerService, JwtStrategy],
 })
