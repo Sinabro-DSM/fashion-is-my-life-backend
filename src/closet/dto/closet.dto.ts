9d352c08c37d4786f52c15cd60f7a215c87922ee
Seung jun <poodle2405@gmail.com>
2021-12-22 14:48:05 +0900

    UPDATE: closet dto

--- src/closet/dto/closet.dto.ts
+++ src/closet/dto/closet.dto.ts
@@ -1,8 +1,21 @@
+import { IsArray, IsDate, IsNumber, IsString } from 'class-validator';
+
 export class closetResponseData {
+  @IsString()
   nickname?: string;
+
+  @IsString()
   title?: string;
-  picture?: string[];
+
+  @IsString()
+  picture?: string;
+
+  @IsArray()
   tags?: string[];
+
+  @IsNumber()
   hanger?: number;
+
+  @IsDate()
   createAt?: Date;
 }
