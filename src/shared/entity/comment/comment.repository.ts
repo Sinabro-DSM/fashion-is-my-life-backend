0788c28feb582eed3e5329b4e477667ff1ec20c2
Seung jun <poodle2405@gmail.com>
2021-12-23 22:27:24 +0900

    UPDATE: comment repository

--- src/shared/entity/comment/comment.repository.ts
+++ src/shared/entity/comment/comment.repository.ts
@@ -32,4 +32,26 @@ export class CommentRepository extends Repository<Comment> {
       .where('comment.comment_id = :comment_id', { comment_id })
       .execute();
   }
+
+  async checkExistComment(comment_id: number): Promise<boolean> {
+    const comment = await this.createQueryBuilder('comment')
+      .select('comment.comment_id')
+      .where('comment.comment_id = :comment_id', { comment_id })
+      .getOne();
+    if (comment) {
+      return true;
+    }
+    return false;
+  }
+
+  async checkAuthority(user_id: number): Promise<boolean> {
+    const checkAuthority = await this.createQueryBuilder('comment')
+      .select('comment.commect_id')
+      .where('comment.user_id = :user_id', { user_id })
+      .getOne();
+    if (checkAuthority) {
+      return true;
+    }
+    return false;
+  }
 }
