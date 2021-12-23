0d2043b4eb9aa6dd3451e91f1f268fd2d338936a
Seung jun <poodle2405@gmail.com>
2021-12-23 15:04:28 +0900

    UPDATE: closet service

--- src/closet/closet.service.ts
+++ src/closet/closet.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from 'src/shared/entity/post/post.entity';
 import { PostRepository } from 'src/shared/entity/post/post.repository';
+import { User } from 'src/shared/entity/user/user.entity';
+import { UserRepository } from 'src/shared/entity/user/user.repository';
 import { NotFoundUserIdError } from 'src/shared/exception/exception.index';
 import { closetResponseData } from './dto/closet.dto';
 
@@ -10,10 +12,12 @@ export class ClosetService {
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: PostRepository,
+    @InjectRepository(User)
+    private readonly userRepository: UserRepository,
   ) {}
 
   public async closet(user_id: number): Promise<closetResponseData> {
-    if (!(await this.postRepository.findOne({ user_id: user_id }))) {
+    if (await this.userRepository.checkExistUser(user_id)) {
       throw NotFoundUserIdError;
     }
     const myCloset = await this.postRepository.closet(user_id);
